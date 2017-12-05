2851d7db1c513037024ddcac6c956ccd8a704c45
kevinptx <kevin.peery@gmail.com>
2017-12-05 16:09:30 -0600

    comments added for foreach-filter-map.js

--- foreach-filter-map.js
+++ foreach-filter-map.js
@@ -6,10 +6,14 @@ Use the filter method to remove any items where the turtle moves backwards or to
 
 const movements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
+//whatever condition is true, it will return those values. Whatever doesn't match what we are checking against is discarded.
     filteredMovements = movements.filter(m => m[0] >= 0 && m[1] >=0);
-    totalSteps = filteredMovements.map(m => m[0]+m[1]);
+
+  //for every element in an array, .map is applying the function we're passing. In this case we're passing an arrow function adding the first element in the array + the second element in the array. For every element in the array, we're passing the array witin it and then the result is that it returns an array to us with the result of the function that was passed.
+    totalSteps = filteredMovements.map(arr => arr[0]+arr[1]);
 
 console.log(filteredMovements);
 console.log(totalSteps);
 
+//for each is applying the function to every element as well. We're saying for every element in the given array, apply the function console.log. Difference between .forEach() and .map() is that .map returns an array whereas forEach doesn't. It simply applies the function that you give it.
 totalSteps.forEach(val => console.log(val));
