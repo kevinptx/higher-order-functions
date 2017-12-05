a2bc3063652e5ef927d9989d43b3c61371c0481f
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-05 12:30:59 -0800

    Add functions as arguments part 2

--- functions-as-arguments.js
+++ functions-as-arguments.js
@@ -27,3 +27,33 @@ function goodbye(){
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+/*
+  Functions as arguments (2)
+
+*/
+
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn){
+    let newArray = [];
+    for(i = 0; i < arr.length; i++){
+        // bug
+        if (fn(arr[i])) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+const pets = ['Fido', 'Fifi', 'Pluto', 'Roger'];
+console.log(filter(pets, name => name[0] === 'R'));
